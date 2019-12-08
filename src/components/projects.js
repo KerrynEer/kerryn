4e9bd68c45c92f036030bb122ccafc16dadf14c6
KerrynEer <KerrynEer@users.noreply.github.com>
2019-12-08 16:19:13 +0800

    Updates to projects
    add sdr project

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,6 +15,7 @@ class Projects extends Component{
 					{this.showPersonalPortfolioWebsite()}
 					{this.showSlapYourEnermyGameProject()}
 					{this.showKnowItAllProject()}
+					{this.showSDRLeaksProject()}
 				</div>
 			)
 		} else if (this.state.activeTab === 1) {
@@ -36,19 +37,24 @@ class Projects extends Component{
 					{this.showKnowItAllProject()}
 				</div>
 			)
+		} else if (this.state.activeTab === 4) {
+			return (
+				<div className="projects-grid">
+					{this.showSDRLeaksProject()}
+				</div>
+			)
 		}
 	}
 
 	/* savemyCAP web project */
 	showSavemycapProject() {
 		return (
-			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginBottom:'10px'}}>
+			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginTop:'10px'}}>
 				<CardTitle expand style={{color: 'black', height: '280px', background: 'url(images/savemycap_logo.png) center / cover'}} >SaveMyCAP</CardTitle>
 				<CardText>
 					<p>
 					An easy-to-use web application that aims to help students keep track of their academic results and facilitate target setting. This is my very first self-directed software development project! 
 					</p>
-					<br></br>
 					<br></br>
 					<p>Built with a friend, using Meteor, MongoDB, NodeJS, Heroku, HTML and CSS. </p>
 				</CardText>
@@ -63,13 +69,12 @@ class Projects extends Component{
 	/* Personal portfolio website */
 	showPersonalPortfolioWebsite() {
 		return (
-			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginBottom:'10px'}}>
+			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginTop:'10px'}}>
 				<CardTitle expand style={{color: 'white', height: '280px', background: 
 				'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}} >
 				Personal Portfolio Website</CardTitle>
 				<CardText>
 					<p>Yes this is the one you are looking at right now! Self-taught by following online tutorials and exploring on my own!</p>
-					<br></br>
 					<br></br>
 					<p>Built using React, NodeJS, HTML and CSS, hosted on Heroku.</p>
 				</CardText>
@@ -84,13 +89,12 @@ class Projects extends Component{
 	/* Slap your enemy game */
 	showSlapYourEnermyGameProject() {
 		return (
-			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginBottom:'10px'}}>
+			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginTop:'10px'}}>
 				<CardTitle expand style={{color: 'black', height: '280px', background: 'url(images/slapyourenermy_logo.png) center / cover'}} >Slap Your Enermy</CardTitle>
 				<CardText>
 					<p>
 					A highly addictive game to relieve one's stress by slapping as fast as you can within 10 seconds. 
 					</p>
-					<br></br>
 					<br></br>
 					<p>Built during a 24-hour hackathon (NUS Hack and Roll 2019) in a team of 4 using Phaser. </p>
 				</CardText>
@@ -102,18 +106,37 @@ class Projects extends Component{
 		);
 	}
 
+	/* Software Defined Radio Leaks */
+	showSDRLeaksProject() {
+		return (
+			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginTop:'20px'}}>
+				<CardTitle expand style={{color: 'black', height: '220px', background: 'url(images/sdrLeak.png) center / contain no-repeat '}} >Software Defined Radio Leaks</CardTitle>
+				<CardText>
+					<p>
+					Using a Software Defined Radio (SDR) - HackRF One, along with other tools like GNU Radio and Gqrx, we sniffed wireless transmissions between a keyboard (Logitech K270 Wireless Keyboard) and a computer. We managed to achieve a partial break - able to detect and capture keypresses on the keyboard, but faced difficulty in decrypting these keypresses to obtain valuable information without the private key.
+					</p>
+					<br></br>
+					<p>All 4 of us are proud to be awarded 1st Prize in NUS School of Computing Term Project Showcase (15th STEPS) for CS3235 Computer Security.</p>
+				</CardText>
+				<CardActions border>
+					<Button colored> <a href="Software Defined Radio Leaks.pdf" download>Project Poster</a> </Button>
+					<Button colored> <a href="CS3235 Group 20 Final Report.pdf" download>Project Report</a>  </Button>
+				</CardActions>
+			</Card>
+		);
+	}
+
 	/* Know-It-All Software Engineering Project */
 	showKnowItAllProject() {
 		return (
-			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginBottom:'10px'}}>
-				<CardTitle expand style={{color: 'black', height: '280px', background: 'url(images/knowitall_logo.png) center / contain no-repeat '}} ></CardTitle>
+			<Card shadow= {5} style= {{minWidth: '550', margin:'auto', marginTop:'20px'}}>
+				<CardTitle expand style={{color: 'black', height: '200px', background: 'url(images/knowitall_logo.png) center / contain no-repeat '}} ></CardTitle>
 				<CardText>
 					<p>
 					A flashcard management app designed to help medical students store, organise and share their learning material, integrated with a test session feature and scoring system to allow for 
 					a more efficient rote learning process. I was in charge of the test session feature. We had to apply Software Engineering Principles and Patterns when implementing it and also write detailed documentation, 
 					which includes sequence diagrams and architecture diagrams in the developer guide, as well as user guide, non-functional requirements etc. Go over to my Project Portfolio to see my contributions!
 					</p>
-					<br></br>
 					<br></br>
 					<p>In a team of 5, we morphed a command line interface addressbook into Know-It-All for NUS CS2103T Software Engineering Project. It is written in Java and GUI is created with JavaFX. It has about 10kLoC.</p>
 				</CardText>
@@ -134,6 +157,7 @@ class Projects extends Component{
 					<Tab><h4>Web</h4></Tab>
 					<Tab><h4>Game</h4></Tab>
 					<Tab><h4>Software Engin</h4></Tab>
+					<Tab><h4>Security</h4></Tab>
 				</Tabs>
 
 				<Grid>
