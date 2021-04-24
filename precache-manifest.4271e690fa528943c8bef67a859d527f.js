20561f7a86578a0e1ef42b85a4f7abb19a68aefd
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2021-04-24 18:11:34 +0800

    Updates

---
+++ precache-manifest.4271e690fa528943c8bef67a859d527f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683a1979e0c0f1d51e4a509716f9eeef",
+    "url": "/kerryn/index.html"
+  },
+  {
+    "revision": "b32bec74e6f9f9913938",
+    "url": "/kerryn/static/css/2.34549731.chunk.css"
+  },
+  {
+    "revision": "82dbfe6977b672dfbf79",
+    "url": "/kerryn/static/css/main.a5ed5b48.chunk.css"
+  },
+  {
+    "revision": "b32bec74e6f9f9913938",
+    "url": "/kerryn/static/js/2.27c8eb1b.chunk.js"
+  },
+  {
+    "revision": "82dbfe6977b672dfbf79",
+    "url": "/kerryn/static/js/main.f06bcc2a.chunk.js"
+  },
+  {
+    "revision": "ff651b1e8ea94232a0c0",
+    "url": "/kerryn/static/js/runtime~main.516eadcf.js"
+  },
+  {
+    "revision": "783b8479a78f99b649282d44ec8c1c70",
+    "url": "/kerryn/static/media/patrickhand-regular-webfont.783b8479.woff2"
+  },
+  {
+    "revision": "787de67a354ce6de724cebefc02918a7",
+    "url": "/kerryn/static/media/patrickhand-regular-webfont.787de67a.woff"
+  },
+  {
+    "revision": "4026301997554014cc24c9b562f0ec42",
+    "url": "/kerryn/static/media/vayenthasans-webfont.40263019.woff"
+  },
+  {
+    "revision": "6600b80bbd6887ce1056fd7d142139e0",
+    "url": "/kerryn/static/media/vayenthasans-webfont.6600b80b.woff2"
+  },
+  {
+    "revision": "0be0b9ef1e0d23fa3c8690d88ee19a88",
+    "url": "/kerryn/static/media/vayenthascript-webfont.0be0b9ef.woff"
+  },
+  {
+    "revision": "638aec1db8cb980fe5d5b95584aaca9a",
+    "url": "/kerryn/static/media/vayenthascript-webfont.638aec1d.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1367e7a417838bb9c536ee3a8e73b20.js"
+  "/kerryn/precache-manifest.4271e690fa528943c8bef67a859d527f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kerryn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
